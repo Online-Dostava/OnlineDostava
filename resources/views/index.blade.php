1a29a9a6bc028b837becaba0852b4dd3bbb02da9
mstojic <74541613+mstojic@users.noreply.github.com>
2021-02-03 09:06:36 +0100

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -43,7 +43,7 @@
 	       
                                    
           <li class="nav-item">
-            <a class="nav-link" href="https://drive.google.com/file/d/1JxQXJOkeSdKxCAJFWhht2MeMlyEDW4or/view" target="_blank">Vizija</a>
+            <a class="nav-link" href="https://drive.google.com/file/d/1ZKgPPSsHvFJ8I8zglqKkIHNbGCIM-6-z/view?usp=sharing" target="_blank">Vizija</a>
         </li>     
         </ul>
         
